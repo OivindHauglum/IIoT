5ac003f317cb485270839df0211b225289b1e2db
OivindHauglum <ohauglum@gmail.com>
2026-05-09 20:53:22 +0200

    Delete gui directory

--- gui/Hcsr04.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "esp_err.h"
-
-esp_err_t hcsr04_init(void);
-
-// temp_c: romtemperatur fra DS18B20 for kompensasjon
-// Send -1000.0f om temperatur ikke er tilgjengelig
-float hcsr04_read_cm(float temp_c);
